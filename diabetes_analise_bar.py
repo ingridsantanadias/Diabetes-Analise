297943e24c914651cb746a4393207905c38edc8f
Ingrid Santana <santanaingriddias@gmail.com>
2026-05-18 13:20:03 -0300

    Update diabetes_analise_bar.py

--- diabetes_analise_bar.py
+++ diabetes_analise_bar.py
@@ -5,7 +5,7 @@ plt.figure(figsize=(10, 6))
 ax = sns.barplot(data=comp_melted, x='Outcome', y='Valor', hue='Metrica', palette=paleta_rosa) #ax=eixos
 plt.title('Comparação: Média de Glicose vs Pressão Arterial')
 plt.xticks([0, 1], ['Sem Diabetes', 'Com Diabetes'])
-# Altera os textos e o título da legenda manualmente 
+# Altera os textos e o título da legenda
 handles, _ = ax.get_legend_handles_labels()
 plt.legend(handles=handles, title='Métricas', labels=['Média de Glicose', 'Média de Pressão Arterial'])
 sns.despine()
